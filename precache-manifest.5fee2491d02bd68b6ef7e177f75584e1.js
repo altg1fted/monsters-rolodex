31986aaac30d57905475daaccf3d735f7a1a5a00
Александр Газарян <ceo@MBP-Aleksandr.Dlink>
2022-04-02 21:25:49 +0300

    Updates

---
+++ precache-manifest.5fee2491d02bd68b6ef7e177f75584e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec15ab1147f0dbc61e438796c72f32c0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5353c901dc7c5ce65565",
+    "url": "/monsters-rolodex/static/css/main.8ddf1634.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2408c5e3ebde9ee289f9",
+    "url": "/monsters-rolodex/static/js/3.b2503d42.chunk.js"
+  },
+  {
+    "revision": "5353c901dc7c5ce65565",
+    "url": "/monsters-rolodex/static/js/main.abd9c848.chunk.js"
+  },
+  {
+    "revision": "5bfbb516207a6d29b863",
+    "url": "/monsters-rolodex/static/js/runtime-main.34519866.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/monsters-rolodex/static/media/logo.06e73328.svg"
+  }
+]);
